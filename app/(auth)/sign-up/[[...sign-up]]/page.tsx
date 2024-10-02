b046465af34f8fe03b5aa3fe50dafbed26218103
Avinash Pandey <avinash.12009084@lpu.in>
2024-10-03 00:21:53 +0530

    Prisma & Postgres DB & clerk Auth

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,10 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 export default function Page() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
